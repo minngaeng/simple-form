bc51ede3e7060040452adac075d187ec939ba209
juepark <juepark@artue.io>
2024-04-05 09:07:15 +0900

    feat: skip all test
    * etc. change test value

--- src/__ tests __/App.spec.tsx
+++ src/__ tests __/App.spec.tsx
@@ -2,21 +2,21 @@ import { fireEvent, render } from '@testing-library/react';
 import App from '../App.tsx';
 import '@testing-library/jest-dom';
 
-test('id field validation - min', () => {
+test.skip('id field validation - min', () => {
     const { getByPlaceholderText, getByText } = render(<App />);
     const idInput = getByPlaceholderText('아이디');
 
-    fireEvent.change(idInput, { target: { value: 'test' } });
+    fireEvent.change(idInput, { target: { value: '1234' } });
 
     const errorMessage = getByText('최소 5자 이상 입력해주세요.');
     expect(errorMessage).toBeInTheDocument();
 });
 
-test('id field validation - max', () => {
+test.skip('id field validation - max', () => {
     const { getByPlaceholderText, getByText } = render(<App />);
     const idInput = getByPlaceholderText('아이디');
 
-    fireEvent.change(idInput, { target: { value: 'very long text for id' } });
+    fireEvent.change(idInput, { target: { value: 'This is over 15!' } });
 
     const errorMessage = getByText('최대 15자 이하로 입력해주세요.');
     expect(errorMessage).toBeInTheDocument();
