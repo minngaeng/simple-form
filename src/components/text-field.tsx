0741b7b8602cb06f63059658d22103432ea93124
minngaeng <minngaeng@gmail.com>
2024-05-18 13:43:50 +0900

    ✨ validate 진행 함수 추가

--- src/components/text-field.tsx
+++ src/components/text-field.tsx
@@ -20,6 +20,23 @@ const TextField = ({
         // 1. validate 함수가 주어진 경우, validate 함수를 실행하여 에러 메시지를 설정합니다.
         // 2. error가 있는 경우 setError 함수를 호출하여 에러 메시지를 설정합니다.
         // 3. error가 없는 경우, success를 true로 설정합니다.
+
+        /**
+         * 민경 : 아래 코드보다 더 좋은 코드가 있을지 고민하고 있습니다.
+         */
+        if (validate) {
+            let validateErr = { success: true, message: '' };
+            for (const validator of validate) {
+                const result = validator(v);
+                if (!result.success) {
+                    validateErr = { ...result, message: result.message || '' };
+                    break;
+                }
+            }
+            if (setError) {
+                setError(validateErr);
+            }
+        }
     };
 
     return (
