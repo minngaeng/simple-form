0eb37c391a5ef25778c55596ec2361ee5dc57775
minngaeng <minngaeng@gmail.com>
2024-05-25 08:15:14 +0900

    불필요한 콘솔 삭제 및 변수 명 변경

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -46,30 +46,28 @@ export const max = (maxLength: number) => {
     };
 };
 
-/**
- * 고차함수 숙지 후 추후 삭제 예정
- * */
-/*const checkMin = (value: string) => {
-    if (value.length > 5) {
-        return {
-            success: true,
-        };
-    } else {
-        return {
-            success: false,
-            message: '최소 5자 이상 입력해주세요.',
-        };
-    }
-};
-*/
-
-export const email = (v: string) => {
+export const emailValidation = (v: string) => {
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
     if (!emailRegex.test(v)) {
         return {
             success: false,
             message: '이메일 형식에 맞게 입력해주세요.',
+        };
+    }
+
+    return {
+        success: true,
+    };
+};
+
+export const passwordValidation = (v: string) => {
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$/;
+
+    if (!passwordRegex.test(v)) {
+        return {
+            success: false,
+            message: '영문, 숫자를 포함하여 8자 이상 입력해주세요.',
         };
     }
 
