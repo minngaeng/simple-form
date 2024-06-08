de854edc473ecd78970c2b29030b5fe13f7730a4
minngaeng <minngaeng@gmail.com>
2024-06-09 01:21:10 +0900

    formik 과 yup 사용하여 form update

--- src/components/text-field.tsx
+++ src/components/text-field.tsx
@@ -1,59 +1,19 @@
-import { FieldProps } from '../types';
+import React from 'react';
+import { useField } from 'formik';
 
-/*
- * TextField 컴포넌트는 input 요소를 렌더링하고, 에러 메시지를 표시합니다.
- **/
-const TextField = ({
-    validate,
-    error = {
-        success: true,
-    },
-    value,
-    setValue,
-    setError,
-    ...rest
-}: FieldProps) => {
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const v = e.target.value;
-        setValue?.(v);
-        // TODO: validation
-        // 1. validate 함수가 주어진 경우, validate 함수를 실행하여 에러 메시지를 설정합니다.
-        // 2. error가 있는 경우 setError 함수를 호출하여 에러 메시지를 설정합니다.
-        // 3. error가 없는 경우, success를 true로 설정합니다.
+interface TextFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
+    name: string;
+}
 
-        if (validate) {
-            let validateErr = { success: true, message: '' };
-
-            for (const validator of validate) {
-                const result = validator(v);
-
-                if (!result.success) {
-                    validateErr = {
-                        ...result,
-                        success: false,
-                        message: result.message || '',
-                    };
-
-                    break;
-                }
-            }
-
-            if (setError) {
-                setError(validateErr);
-            } else {
-                validateErr.success = true;
-            }
-        }
-    };
+const TextField: React.FC<TextFieldProps> = ({ ...props }) => {
+    const [field, meta] = useField(props);
 
     return (
-        <div className={'text-field'}>
-            <input value={value} onChange={handleChange} {...rest} />
-            <div className={'error-message'}>
-                {!error.success && (
-                    <p id={`${rest.name}-error`}>{error.message}</p>
-                )}
-            </div>
+        <div className="text-field">
+            <input {...field} {...props} />
+            {meta.touched && meta.error ? (
+                <div className="error-message">{meta.error}</div>
+            ) : null}
         </div>
     );
 };
