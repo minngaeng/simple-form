f35b84cdd711c9ccfde342f960748d0e0461f3f9
humonnom <juepark42@gmail.com>
2024-06-15 11:47:00 +0900

    feat: 코드 수정전 test 코드 주석처리
    Co-authored-by: minngaeng <minngaeng@users.noreply.github.com>

--- src/__ tests __/App2.spec.tsx
+++ src/__ tests __/App2.spec.tsx
@@ -3,7 +3,7 @@ import App from '../App.tsx';
 import '@testing-library/jest-dom';
 
 // TODO: 아래 테스트를 통과하도록 name field validation을 구현하세요.
-test('name field validation - required', () => {
+test.skip('name field validation - required', () => {
     const { getByPlaceholderText, getByText } = render(<App />);
     const nameInput = getByPlaceholderText('이름');
 
@@ -23,7 +23,7 @@ test('name field validation - required', () => {
 // 도메인명 (Domain Name): @ 기호 뒤에 위치하며, 이메일 서비스를 제공하는 서버를 나타냅니다. 예를 들어, "example"가 도메인명입니다.
 // 최상위도메인 (Top-Level Domain, TLD): 도메인명 뒤에 마침표(.)로 구분되어 나타나며, 도메인의 목적과 종류를 나타냅니다. 국가코드최상위도메인(ccTLD)과 일반최상위도메인(gTLD)이 있습니다. 예를 들어, "com"은 상업용 gTLD입니다.
 
-test('email field validation - invalid local part', () => {
+test.skip('email field validation - invalid local part', () => {
     const { getByPlaceholderText, getByText } = render(<App />);
     const emailInput = getByPlaceholderText('이메일');
 
@@ -33,7 +33,7 @@ test('email field validation - invalid local part', () => {
     expect(errorMessage).toBeInTheDocument();
 });
 
-test('email field validation - missing @', () => {
+test.skip('email field validation - missing @', () => {
     const { getByPlaceholderText, getByText } = render(<App />);
     const emailInput = getByPlaceholderText('이메일');
 
@@ -43,7 +43,7 @@ test('email field validation - missing @', () => {
     expect(errorMessage).toBeInTheDocument();
 });
 
-test('email field validation - invalid domain name', () => {
+test.skip('email field validation - invalid domain name', () => {
     const { getByPlaceholderText, getByText } = render(<App />);
     const emailInput = getByPlaceholderText('이메일');
 
@@ -79,3 +79,19 @@ test('password field validation - required', () => {
     );
     expect(errorMessage).toBeInTheDocument();
 });
+
+test('password field and password-confirm field validation - match', () => {
+    const { getByPlaceholderText, getByText } = render(<App />);
+
+    const passwordInput = getByPlaceholderText('비밀번호');
+    const passwordConfirmInput = getByPlaceholderText('비밀번호 확인');
+
+    fireEvent.change(passwordInput, { target: { value: 'abc1234' } });
+    fireEvent.change(passwordConfirmInput, { target: { value: 'ab12345' } });
+    expect(getByText('비밀번호가 일치하지 않습니다.')).toBeInTheDocument();
+});
+
+// step
+// 1. password, passwordConfirm 이 일치하는지 여부에 대해서 잘 체크하는지 테스트를 작성
+// 2. test가 통과하도록 passwordConfirm validation을 구현
+// 3. formik을 모방하여 Form 컴포넌트 수정, childre을 renderProps로 변경
