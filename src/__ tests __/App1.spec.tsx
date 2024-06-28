4647c816dc8798c676bbd51ab35df7b270bad98c
minngaeng <minkyung@uzurotech.com>
2024-06-28 15:47:00 +0900

    test skip 처리

---
+++ src/__ tests __/App1.spec.tsx
@@ -0,0 +1,49 @@
+import { fireEvent, render } from '@testing-library/react';
+import App from '../App.tsx';
+import '@testing-library/jest-dom';
+
+// TODO: 모든 테스트 통과하게 만들기
+// skip을 제거하고 테스트를 실행해보세요.
+// 한번에 하나씩 순서대로 진행하세요.
+//  test.skip -> test
+
+test('id field validation - min', () => {
+    const { getByPlaceholderText, getByText } = render(<App />);
+    const idInput = getByPlaceholderText('아이디');
+
+    fireEvent.change(idInput, { target: { value: '1234' } });
+
+    const errorMessage = getByText('최소 5자 이상 입력해주세요.');
+    expect(errorMessage).toBeInTheDocument();
+});
+
+test('id field validation - max', () => {
+    const { getByPlaceholderText, getByText } = render(<App />);
+    const idInput = getByPlaceholderText('아이디');
+
+    fireEvent.change(idInput, { target: { value: 'This is over 15!' } });
+
+    const errorMessage = getByText('최대 15자 이하로 입력해주세요.');
+    expect(errorMessage).toBeInTheDocument();
+});
+
+test.skip('name field exists', () => {
+    const { getByPlaceholderText } = render(<App />);
+    const nameInput = getByPlaceholderText('이름');
+
+    expect(nameInput).toBeInTheDocument();
+});
+
+test.skip('email field exists', () => {
+    const { getByPlaceholderText } = render(<App />);
+    const emailInput = getByPlaceholderText('이메일');
+
+    expect(emailInput).toBeInTheDocument();
+});
+
+test.skip('password-confirm field exists', () => {
+    const { getByPlaceholderText } = render(<App />);
+    const passwordConfirmInput = getByPlaceholderText('비밀번호 확인');
+
+    expect(passwordConfirmInput).toBeInTheDocument();
+});
--- src/__ tests __/App2.spec.tsx
+++ src/__ tests __/App2.spec.tsx
@@ -2,7 +2,7 @@ import { fireEvent, render } from '@testing-library/react';
 import App from '../App.tsx';
 import '@testing-library/jest-dom';
 
-test('name field validation - required', () => {
+test.skip('name field validation - required', () => {
     const { getByPlaceholderText, getByText } = render(<App />);
     const nameInput = getByPlaceholderText('이름');
 
@@ -22,7 +22,7 @@ test('name field validation - required', () => {
 // 도메인명 (Domain Name): @ 기호 뒤에 위치하며, 이메일 서비스를 제공하는 서버를 나타냅니다. 예를 들어, "example"가 도메인명입니다.
 // 최상위도메인 (Top-Level Domain, TLD): 도메인명 뒤에 마침표(.)로 구분되어 나타나며, 도메인의 목적과 종류를 나타냅니다. 국가코드최상위도메인(ccTLD)과 일반최상위도메인(gTLD)이 있습니다. 예를 들어, "com"은 상업용 gTLD입니다.
 
-test('email field validation - invalid local part', () => {
+test.skip('email field validation - invalid local part', () => {
     const { getByPlaceholderText, getByText } = render(<App />);
     const emailInput = getByPlaceholderText('이메일');
 
@@ -32,7 +32,7 @@ test('email field validation - invalid local part', () => {
     expect(errorMessage).toBeInTheDocument();
 });
 
-test('email field validation - missing @', () => {
+test.skip('email field validation - missing @', () => {
     const { getByPlaceholderText, getByText } = render(<App />);
     const emailInput = getByPlaceholderText('이메일');
 
@@ -42,7 +42,7 @@ test('email field validation - missing @', () => {
     expect(errorMessage).toBeInTheDocument();
 });
 
-test('email field validation - invalid domain name', () => {
+test.skip('email field validation - invalid domain name', () => {
     const { getByPlaceholderText, getByText } = render(<App />);
     const emailInput = getByPlaceholderText('이메일');
 
@@ -52,7 +52,7 @@ test('email field validation - invalid domain name', () => {
     expect(errorMessage).toBeInTheDocument();
 });
 
-test('password field validation - required', () => {
+test.skip('password field validation - required', () => {
     const { getByPlaceholderText, getByText } = render(<App />);
     const passwordInput = getByPlaceholderText('비밀번호');
 
@@ -77,7 +77,7 @@ test('password field validation - required', () => {
     expect(errorMessage).toBeInTheDocument();
 });
 
-test('password field and password-confirm field validation - match', () => {
+test.skip('password field and password-confirm field validation - match', () => {
     const { getByPlaceholderText, getByText } = render(<App />);
 
     const passwordInput = getByPlaceholderText('비밀번호');
